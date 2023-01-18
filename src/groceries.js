c7e6f202d25070879813c4421038388bcfd5c4b1
parvathi <sriparvathi.kalakoti@gmail.com>
2023-01-17 23:07:59 -0600

    working app

---
+++ src/groceries.js
@@ -0,0 +1,33 @@
+
+export default [
+    {
+      item: 'Instant Oatmeal',
+      brand: 'Quarker',
+      units: '40pack',
+      quantity: 10,
+      isPurchased: false
+    },
+    
+    {
+      item: 'Bread',
+      brand: 'Natures Own',
+      units: '1pack',
+      quantity: 5,
+      isPurchased: false
+    },
+    {
+        item: 'Easy Mac',
+        brand: 'Kraft',
+        units: '15pack',
+        quantity: 5,
+        isPurchased: false
+      },
+      {
+        item: 'Granola Bars',
+        brand: 'Nature Valley',
+        units: '25pack',
+        quantity: 5,
+        isPurchased: false
+      },
+  ]
+  
